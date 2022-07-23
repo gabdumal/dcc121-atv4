c09d60913ffbbda94601cf2f0bd5b9a3c7f49c46
Gabriel Malosto <professional@gabdumal.com>
2022-07-23 15:07:17 -0300

    Muda ouvinte para clique no fazendeiro

--- index.js
+++ index.js
@@ -6,13 +6,21 @@ const embarcacaoEstado = {
 
 // Captura os elementos do DOM
 const embarcacao = document.querySelector("#embarcacao");
+const fazendeiro = embarcacao.firstElementChild.firstElementChild;
 const margemEsq = document.querySelector("#margemEsq");
 const margemDir = document.querySelector("#margemDir");
 
 // Ouvintes
-embarcacao.addEventListener("click", clickListenerEmbarcacao);
-function clickListenerEmbarcacao() {
-  console.log(embarcacao);
-  console.log(margemEsq);
-  console.log(margemDir);
+fazendeiro.addEventListener("click", clickListenerFazendeiro);
+function clickListenerFazendeiro() {
+  // Troca de margem
+  embarcacaoEstado.margem === "esq"
+    ? (embarcacaoEstado.margem = "dir")
+    : (embarcacaoEstado.margem = "esq");
+
+  console.log(embarcacaoEstado);
+  console.log(fazendeiro);
+  //   console.log(embarcacao);
+  //   console.log(margemEsq);
+  //   console.log(margemDir);
 }
