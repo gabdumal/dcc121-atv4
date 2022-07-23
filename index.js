ef61dfce14f3f7fb3c4f846625cd7f2c808c646b
Gabriel Malosto <professional@gabdumal.com>
2022-07-23 14:53:59 -0300

    Captura embarcação no JS

---
+++ index.js
@@ -0,0 +1,10 @@
+// Estado
+const embarcacaoEstado = {
+  margem: "esq",
+  carga: "",
+};
+
+// Captura os elementos do DOM
+const embarcacao = document.querySelector("#embarcacao");
+
+console.log(embarcacao);
